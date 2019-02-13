2e180b529a97d4007f70e99d99874e5ff1e44188
Hugh Broome <hughdylanbroome@gmail.com>
2019-02-13 14:08:53 -0500

    Initial Commit

---
+++ CannaTrack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CannaTrack
+//
+//  Created by Hugh Broome on 2/13/19.
+//  Copyright © 2019 Lygre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
