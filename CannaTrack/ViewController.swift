d3addffd6f5b7bc32a3ae53d3e634e25339bc686
Hugh Broome <hughdylanbroome@gmail.com>
2019-02-13 23:52:33 -0500

    PArsing Effects resolved. Just a strange formatting thing.

--- CannaTrack/ViewController.swift
+++ CannaTrack/ViewController.swift
@@ -20,21 +20,60 @@ class DetailViewConstroller: UIViewController {
 
 class SearchViewController: UIViewController {
 
+	var strainsArray: Strain?
+	var effectsArray: [Effects]?
+
 	@IBOutlet var searchTextField: UITextField!
-
-
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
+		let url = "https://strainapi.evanbusse.com/oJ5GvWc/searchdata/effects/"
+		guard let urlObj = URL(string: url) else { return }
+
+		URLSession.shared.dataTask(with: urlObj) {(data, response, error) in
+
+			guard let data = data else { return }
+
+			do {
+				self.effectsArray = try JSONDecoder().decode([Effects].self, from: data)
+				print("data parsed from strain database")
+			} catch let jsonError {
+				print("Error serializing json: ", jsonError)
+			}
+
+			}.resume()
 	}
+
+	func searchWeed(using strainString: String) -> [Strain] {
+		var strainMatches: [Strain]?
+
+
+
+		return strainMatches!
+	}
+
+	@IBAction func searchClicked(_ sender: UIButton) {
+		guard let strainSearchString = searchTextField.text else { return }
+
+	}
+
 }
 
-struct Strain {
-	var id: Int
-	var race: String
-	var flavors: [String]
-	var effects: [String: [String]]
+struct Strain: Decodable {
 
+	let id: Int
+	let name: String
+	let race: String
+	let desc: String
+	let flavors: String?
+	let effects: [String: [String]]?
+
+
+}
+
+struct Effects: Decodable {
+	let effect: String
+	let type: String
 }
 
