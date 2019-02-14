16e3b3a9e4d2fe9645fc0d9dbf740cf2bbc1a4d1
Hugh Broome <hughdylanbroome@gmail.com>
2019-02-14 13:46:19 -0500

    This worked perfectly. And should be able to parse the other API calls fine to by changing all of the properties to optionals

--- CannaTrack/ViewController.swift
+++ CannaTrack/ViewController.swift
@@ -20,29 +20,20 @@ class DetailViewConstroller: UIViewController {
 
 class SearchViewController: UIViewController {
 
-	var strainsArray: Strain?
+	var strainsArray: [Strain]?
 	var effectsArray: [Effects]?
+	var url = "https://strainapi.evanbusse.com/oJ5GvWc/strains/search/name/"
+	let urlForEffectsSearch = "https://strainapi.evanbusse.com/oJ5GvWc/searchdata/effects/"
 
 	@IBOutlet var searchTextField: UITextField!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
-		let url = "https://strainapi.evanbusse.com/oJ5GvWc/searchdata/effects/"
-		guard let urlObj = URL(string: url) else { return }
 
-		URLSession.shared.dataTask(with: urlObj) {(data, response, error) in
 
-			guard let data = data else { return }
 
-			do {
-				self.effectsArray = try JSONDecoder().decode([Effects].self, from: data)
-				print("data parsed from strain database")
-			} catch let jsonError {
-				print("Error serializing json: ", jsonError)
-			}
 
-			}.resume()
 	}
 
 	func searchWeed(using strainString: String) -> [Strain] {
@@ -53,10 +44,39 @@ class SearchViewController: UIViewController {
 		return strainMatches!
 	}
 
+	func searchStrains(using effect: String) {
+		url = "https://strainapi.evanbusse.com/oJ5GvWc/strains/search/name/" + String("\(effect)").trimmingCharacters(in: .whitespaces)
+		guard let urlObj = URL(string: url) else { return }
+
+		URLSession.shared.dataTask(with: urlObj) {(data, response, error) in
+
+			guard let data = data else { return }
+
+			do {
+				self.strainsArray = try JSONDecoder().decode([Strain].self, from: data)
+				print("data parsed from strain database")
+			} catch let jsonError {
+				print("Error serializing json: ", jsonError)
+			}
+
+			}.resume()
+	}
+
+	func refreshUI() {
+		loadViewIfNeeded()
+		print(strainsArray)
+	}
+
 	@IBAction func searchClicked(_ sender: UIButton) {
 		guard let strainSearchString = searchTextField.text else { return }
-
+		searchStrains(using: strainSearchString)
 	}
+
+	@IBAction func reloadView(_ sender: Any) {
+		refreshUI()
+	}
+
+
 
 }
 
@@ -65,12 +85,15 @@ struct Strain: Decodable {
 	let id: Int
 	let name: String
 	let race: String
-	let desc: String
-	let flavors: String?
-	let effects: [String: [String]]?
+	let desc: String?
+//	let flavors: String?
+//	let effects: [String: [String]]?
 
+//	let strain: [String]
 
 }
+
+
 
 struct Effects: Decodable {
 	let effect: String
