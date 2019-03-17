ef2d6a1d0c8215718296ee70249090418e1dfec9
Hugh Broome <hughdylanbroome@gmail.com>
2019-03-17 09:58:30 -0400

    Stuff

--- CannaTrack/Primary Controllers/DynamicProductsViewController.swift
+++ CannaTrack/Primary Controllers/DynamicProductsViewController.swift
@@ -10,8 +10,16 @@ import UIKit
 
 class DynamicProductsViewController: UIViewController {
 
+	let ThrowingThreshold: CGFloat = 900
+	let ThrowingVelocityPadding: CGFloat = 35
+
+	var originalBounds = CGRect.zero
+	var originalCenter = CGPoint.zero
 
 	var snap: UISnapBehavior!
+	var attachment: UIAttachmentBehavior!
+	var pushBehavior: UIPushBehavior!
+	var itemBehavior: UIDynamicItemBehavior!
 	var animator: UIDynamicAnimator!
 	var productViewArray: [ProductView]!
 
@@ -65,16 +73,66 @@ extension DynamicProductsViewController {
 	}
 
 
+	func resetViews() {
+		animator.removeAllBehaviors()
+
+		UIView.animate(withDuration: 0.45) {
+			for productView in self.productViewArray {
+				productView.bounds = self.originalBounds
+				productView.center = self.originalCenter
+				productView.transform = CGAffineTransform.identity
+			}
+		}
+
+	}
+
 	@objc func handlePanForProductView(recognizer: UIPanGestureRecognizer) {
+		let location = recognizer.location(in: self.view)
+		let locationCenterView = view.center
+//		let boxLocation = recognizer.location(in: self.)
+		guard let productViewToTranslate = recognizer.view else { return }
+
 		switch recognizer.state {
 		case .changed:
 			let translation = recognizer.translation(in: view)
-			guard let productViewToTranslate = recognizer.view else { return }
+
 			productViewToTranslate.center = CGPoint(x: productViewToTranslate.center.x + translation.x, y: productViewToTranslate.center.y + translation.y)
 			recognizer.setTranslation(.zero, in: view)
+
+		case .began:
+			animator.removeAllBehaviors()
+
+			let centerOffset = UIOffset(horizontal: 50, vertical: -50)
+
+			attachment = UIAttachmentBehavior(item: productViewToTranslate, offsetFromCenter: centerOffset, attachedToAnchor: locationCenterView)
+
+//			productViewToTranslate.center = attachment.anchorPoint
+			animator.addBehavior(attachment)
+
+		case .ended:
+			let velocity = recognizer.velocity(in: view)
+			let magnitude = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y))
+			if magnitude > ThrowingThreshold {
+				let pushBehavior = UIPushBehavior(items: productViewArray, mode: .instantaneous)
+				pushBehavior.pushDirection = CGVector(dx: velocity.x / 10, dy: velocity.y / 10)
+				pushBehavior.magnitude = magnitude / ThrowingVelocityPadding
+				self.pushBehavior = pushBehavior
+				animator.addBehavior(pushBehavior)
+
+				let angle = Int(arc4random_uniform(20)) - 10
+
+				itemBehavior = UIDynamicItemBehavior(items: productViewArray)
+				itemBehavior.friction = 0.2
+				itemBehavior.allowsRotation = true
+				itemBehavior.addAngularVelocity(CGFloat(angle), for: productViewToTranslate)
+				animator.addBehavior(itemBehavior)
+			}
+//			else { resetViews() }
 		default:
-			break
+			attachment.anchorPoint = location
+			productViewToTranslate.center = attachment.anchorPoint
 		}
+
 	}
 
 }
