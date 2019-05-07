07147eeac6ec28b567381033ec95fcf51277244b
Hugh Broome <hughdbroome@gmail.com>
2019-05-07 19:44:41 -0400

    attempt to add shadow

--- CannaTrack/Subclasses/AddProductFloatingButton.swift
+++ CannaTrack/Subclasses/AddProductFloatingButton.swift
@@ -48,6 +48,11 @@ class AddProductFloatingButton: UIButton {
 //		self.layer.masksToBounds = true
 		self.clipsToBounds = true
 		self.layer.cornerRadius = self.bounds.size.height / 2
+		self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).CGColor
+		self.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+		self.layer.shadowOpacity = 1.0
+		self.layer.shadowRadius = 0.0
+		self.layer.masksToBounds = false
 	}
 
 
