5395f62586338f3f1e014fe4b765ea9e1534ec42
Hugh Broome <hughdylanbroome@gmail.com>
2019-05-08 12:59:34 -0400

    --CustomCell.swift: • Changed dosesPresentIndicatorView property to its own subclass, DoseIndicatorView, a subclass of UIView • Changed setting the cornerRadius value in layoutSubviews to use bounds for the calculation • Removed layer.masksToBounds = true and set clipsToBounds equal too true. • DoseIndicatorView subclass only overrides bounds property to make it always circular.

--- CannaTrack/Cells/CustomCell.swift
+++ CannaTrack/Cells/CustomCell.swift
@@ -13,7 +13,7 @@ class CustomCell: JTAppleCell {
 
 	@IBOutlet weak var dateLabel: UILabel!
 	@IBOutlet weak var selectedView: UIView!
-	@IBOutlet weak var dosesPresentIndicatorView: UIView!
+	@IBOutlet weak var dosesPresentIndicatorView: DoseIndicatorView!
 
 	var dosesPresentOnDate: Bool! = false
 
@@ -28,8 +28,22 @@ class CustomCell: JTAppleCell {
 		frame.size.width = min(frame.width, frame.height)
 		frame.size.height = frame.width
 		self.selectedView.layer.cornerRadius = frame.width * 0.5
-		self.dosesPresentIndicatorView.layer.cornerRadius = self.dosesPresentIndicatorView.frame.size.height * 0.5
-		self.dosesPresentIndicatorView.layer.masksToBounds = true
+		self.dosesPresentIndicatorView.layer.cornerRadius = self.dosesPresentIndicatorView.bounds.size.height / 2.0
+//		self.dosesPresentIndicatorView.layer.masksToBounds = true
+		self.dosesPresentIndicatorView.clipsToBounds = true
+	}
+
+}
+
+class DoseIndicatorView: UIView {
+
+	override var bounds: CGRect {
+		get { return super.bounds }
+		set(newBounds) {
+			super.bounds = newBounds
+			let newFrameSize = min(newBounds.size.width, newBounds.size.height)
+			self.layer.cornerRadius = newFrameSize / 2.0
+		}
 	}
 
 }
