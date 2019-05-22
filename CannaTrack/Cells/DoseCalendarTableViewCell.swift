c158cb761f61e71e6435ee45abbfacb1535c0db8
Hugh Broome <hughdylanbroome@gmail.com>
2019-05-22 15:08:15 -0400

    --Dynamically resizing tableview cells achieved. Should rewrite all cells in this manner.

--- CannaTrack/Cells/DoseCalendarTableViewCell.swift
+++ CannaTrack/Cells/DoseCalendarTableViewCell.swift
@@ -11,21 +11,82 @@ import UIKit
 class DoseCalendarTableViewCell: UITableViewCell {
 
 
-	@IBOutlet var timeLabel: UILabel!
-	@IBOutlet var productLabel: UILabel!
-	@IBOutlet var strainLabel: UILabel!
+	let timeLabel = makePrimaryLabel()
+	let productLabel = makePrimaryLabel()
+	let strainLabel = makePrimaryLabel()
 
 
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+	private static func makePrimaryLabel() -> UILabel {
+		let label = UILabel()
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.numberOfLines = 0
+		return label
+	}
 
-        // Configure the view for the selected state
-    }
+	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+		super.init(style: style, reuseIdentifier: reuseIdentifier)
+		setUpLabelsAndConstraints()
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		setUpLabelsAndConstraints()
+	}
+
+//	layout
+
+	private func setUpLabelsAndConstraints() {
+		//dynamic font size
+
+		//custom defined font
+
+		guard let palatino = UIFont(name: "Palatino", size: 18) else {
+			fatalError("""
+                Failed to load the "Palatino" font.
+                Since this font is included with all versions of iOS that support Dynamic Type, verify that the spelling and casing is correct.
+                """)
+		}
+
+
+		timeLabel.font = UIFont.preferredFont(forTextStyle: .headline)
+		timeLabel.adjustsFontForContentSizeCategory = true
+
+
+
+		productLabel.font = UIFontMetrics(forTextStyle: .body).scaledFont(for: palatino)
+		productLabel.adjustsFontForContentSizeCategory = true
+
+
+		strainLabel.font = UIFontMetrics(forTextStyle: .body).scaledFont(for: palatino)
+		strainLabel.adjustsFontForContentSizeCategory = true
+
+		contentView.addSubview(timeLabel)
+		contentView.addSubview(strainLabel)
+		contentView.addSubview(productLabel)
+
+		timeLabel.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor).isActive = true
+		timeLabel.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor).isActive = true
+		strainLabel.leadingAnchor.constraint(equalTo: timeLabel.leadingAnchor).isActive = true
+		strainLabel.trailingAnchor.constraint(equalTo: timeLabel.trailingAnchor).isActive = true
+		productLabel.leadingAnchor.constraint(equalTo: timeLabel.leadingAnchor).isActive = true
+		productLabel.trailingAnchor.constraint(equalTo: timeLabel.trailingAnchor).isActive = true
+
+
+
+		timeLabel.firstBaselineAnchor.constraint(equalToSystemSpacingBelow: contentView.layoutMarginsGuide.topAnchor, multiplier: 1).isActive = true
+		strainLabel.firstBaselineAnchor.constraint(equalToSystemSpacingBelow: timeLabel.lastBaselineAnchor, multiplier: 1).isActive = true
+		productLabel.firstBaselineAnchor.constraint(equalToSystemSpacingBelow: strainLabel.lastBaselineAnchor, multiplier: 1).isActive = true
+
+		contentView.layoutMarginsGuide.bottomAnchor.constraint(equalToSystemSpacingBelow: productLabel.lastBaselineAnchor, multiplier: 1).isActive = true
+
+
+
+
+
+
+	}
+
 
 }
